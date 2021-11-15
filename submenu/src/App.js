eda536d30acb66b7ce507ac65d6f50e811edba5b
Raghav <codingkapur@gmail.com>
2021-11-15 16:20:38 +0530

    onMouseOver floating modals

--- submenu/src/App.js
+++ submenu/src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import Submenu from "./Submenu";
 
 function App() {
   return (
     <div className="App">
-     <h1>Submenus</h1>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <Submenu />
     </div>
   );
 }
